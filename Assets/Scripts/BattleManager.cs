227a463945e7ee2ff81952f3b4e4731f40e6c7d1
Unknown <rh845@live.mdx.ac.uk>
2019-12-09 18:27:58 +0000

    V0.9
    Basic Attacking

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BattleManager : MonoBehaviour
 {
@@ -15,9 +16,10 @@ public class BattleManager : MonoBehaviour
         go_EnemyLvl18, go_EnemyLvl19, go_EnemyLvl20;
     public PlayerScript playerScript;
 
-    private int i_PlayerLevel=0, i_EnemyToSpawn, i_EnemiesLevels=0, i=0;
+    private int i_PlayerLevel=0, i_EnemyToSpawn, i_EnemiesLevels=0, i=0, i_EnemyTurn=0;
     private bool b_PlayerTurn=true;
-    private GameObject[] i_Enemies = new GameObject[5];
+    public List<GameObject> i_Enemies;
+    private EnemyScript enemyscript;
 
     // Start is called before the first frame update
     void Start()
@@ -83,64 +85,64 @@ public class BattleManager : MonoBehaviour
             switch (i_EnemyToSpawn)
             {
                 case 1:
-                    i_Enemies[i] = go_EnemyLvl1;
+                    i_Enemies.Add(go_EnemyLvl1);
                     break;
                 case 2:
-                    i_Enemies[i] = go_EnemyLvl2;
+                    i_Enemies.Add(go_EnemyLvl2);
                     break;
                 case 3:
-                    i_Enemies[i] = go_EnemyLvl3;
+                    i_Enemies.Add(go_EnemyLvl3);
                     break;
                 case 4:
-                    i_Enemies[i] = go_EnemyLvl4;
+                    i_Enemies.Add(go_EnemyLvl4);
                     break;
                 case 5:
-                    i_Enemies[i] = go_EnemyLvl5;
+                    i_Enemies.Add(go_EnemyLvl5);
                     break;
                 case 6:
-                    i_Enemies[i] = go_EnemyLvl6;
+                    i_Enemies.Add(go_EnemyLvl6);
                     break;
                 case 7:
-                    i_Enemies[i] = go_EnemyLvl7;
+                    i_Enemies.Add(go_EnemyLvl7);
                     break;
                 case 8:
-                    i_Enemies[i] = go_EnemyLvl8;
+                    i_Enemies.Add(go_EnemyLvl8);
                     break;
                 case 9:
-                    i_Enemies[i] = go_EnemyLvl9;
+                    i_Enemies.Add(go_EnemyLvl9);
                     break;
                 case 10:
-                    i_Enemies[i] = go_EnemyLvl10;
+                    i_Enemies.Add(go_EnemyLvl10);
                     break;
                 case 11:
-                    i_Enemies[i] = go_EnemyLvl11;
+                    i_Enemies.Add(go_EnemyLvl11);
                     break;
                 case 12:
-                    i_Enemies[i] = go_EnemyLvl12;
+                    i_Enemies.Add(go_EnemyLvl12);
                     break;
                 case 13:
-                    i_Enemies[i] = go_EnemyLvl13;
+                    i_Enemies.Add(go_EnemyLvl13);
                     break;
                 case 14:
-                    i_Enemies[i] = go_EnemyLvl14;
+                    i_Enemies.Add(go_EnemyLvl14);
                     break;
                 case 15:
-                    i_Enemies[i] = go_EnemyLvl15;
+                    i_Enemies.Add(go_EnemyLvl15);
                     break;
                 case 16:
-                    i_Enemies[i] = go_EnemyLvl16;
+                    i_Enemies.Add(go_EnemyLvl16);
                     break;
                 case 17:
-                    i_Enemies[i] = go_EnemyLvl17;
+                    i_Enemies.Add(go_EnemyLvl17);
                     break;
                 case 18:
-                    i_Enemies[i] = go_EnemyLvl18;
+                    i_Enemies.Add(go_EnemyLvl18);
                     break;
                 case 19:
-                    i_Enemies[i] = go_EnemyLvl19;
+                    i_Enemies.Add(go_EnemyLvl19);
                     break;
                 case 20:
-                    i_Enemies[i] = go_EnemyLvl20;
+                    i_Enemies.Add(go_EnemyLvl20);
                     break;
             }        
             i_EnemiesLevels += i_EnemyToSpawn;
@@ -148,12 +150,13 @@ public class BattleManager : MonoBehaviour
             i++;
         }
         i = 0;
-        foreach (GameObject s in go_Spawns)
+        while(i<i_Enemies.Count)
         {
-            Instantiate(i_Enemies[i],s.transform);
-            Debug.Log("Spawning enemy " + i + " in spawn " + s);
+            Instantiate(i_Enemies[i], go_Spawns[i].transform);
+            Debug.Log("Spawning enemy " + i + " in spawn " + go_Spawns[i]);
             i++;
         }
+        Debug.Log(i_Enemies.Count + " Enemies");
     }
 
     // Update is called once per frame
@@ -166,18 +169,73 @@ public class BattleManager : MonoBehaviour
                 b.interactable = false;
             }
             //Enemies turn
+            if (i_Enemies.Count > 0)
+            {
+                EnemyAttack();
+            }
+            else
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
         }
-        else
+        else if(b_PlayerTurn)
         {
             foreach (Button b in bu_Buttons)
             {
                 b.interactable = true;
             }
+            if (Input.GetMouseButtonDown(0))
+            {
+                foreach (GameObject go in i_Enemies)
+                {
+                    Debug.Log(go);
+                }
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+                RaycastHit2D hitPoint = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+                if (hitPoint)  
+                {
+                    Attack(hitPoint.transform.gameObject);
+                }
+            }
         }
     }
 
-    public void Attack()
+    public void Attack(GameObject target)
     {
+        enemyscript=target.GetComponent<EnemyScript>();
+        enemyscript.i_Hitpoints -= playerScript.i_Attack;
+        enemyscript.sld_HealthSlider.value = enemyscript.i_Hitpoints;
+        Debug.Log("Enemy HP " + enemyscript.i_Hitpoints);
+        if(enemyscript.i_Hitpoints<=0)
+        {
+            i_Enemies.Remove(target);
+            DestroyObject(target);
+            Debug.Log(target + " dead, " + i_Enemies.Count + " left");
+        }
+        b_PlayerTurn = false;
+    }
 
+    public void EnemyAttack()
+    {
+        Debug.Log("Enemy turn");
+        enemyscript = i_Enemies[i_EnemyTurn].GetComponent<EnemyScript>();
+        playerScript.f_HP -= enemyscript.i_Attack;
+        playerScript.sld_HealthSlider.value = playerScript.f_HP;
+        if (i_EnemyTurn <i_Enemies.Count)
+        {
+            i_EnemyTurn++;
+            while (i_Enemies[i_EnemyTurn]==null)
+            {
+                i_EnemyTurn++;
+            }
+        }
+        else
+        {
+            i_EnemyTurn = 0;
+        }
+        b_PlayerTurn = true;
     }
 }
--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyScript : MonoBehaviour
 {
-    public int i_Hitpoints, i_Level;
+    public int i_Hitpoints, i_Level, i_Attack;
+    public Slider sld_HealthSlider;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        sld_HealthSlider = GetComponentInChildren<Slider>();
+        sld_HealthSlider.maxValue = i_Hitpoints;
+        sld_HealthSlider.value = i_Hitpoints;
     }
 
     // Update is called once per frame
